072ff317e35e0f806aecde410c441e322124adf2
Alejo Arteaga <alejoarte123@icloud.com>
2025-10-07 15:10:03 -0500

    Update

--- randomquotes.py
+++ randomquotes.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 import os
 import random
 
@@ -14,13 +14,11 @@ phrases = [
     "Alone or not you gonna walk forward.",
 ]
 
-
 @app.route('/')
 def get_random_quote():
-    phrase = random.choice(phrases)
-    container_id = os.uname()[1] 
-    return f"{phrase} - Container Id: {container_id}"
-
+ phrase = random.choice(phrases)
+ container_id = os.uname()[1]
+ return f"{phrase} - Container Id: {container_id}" 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
 
