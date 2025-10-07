1b6aa90bd02092a0925bcbe16dc7a7e12e78ed35
Alejo Arteaga <alejoarte123@icloud.com>
2025-10-07 16:15:53 -0500

    Updating SCS

--- randomquotes.py
+++ randomquotes.py
@@ -16,9 +16,10 @@ phrases = [
 
 @app.route('/')
 def get_random_quote():
- phrase = random.choice(phrases)
- container_id = os.uname()[1]
- return f"{phrase} - Container Id: {container_id}" 
+    phrase = random.choice(phrases)
+    container_id = os.uname()[1]
+    return f"{phrase} - Container Id: {container_id}" 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
 
